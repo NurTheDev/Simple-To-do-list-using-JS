949037679153f6c4870d160fe58631e11bae8676
NurTheDev <nur756.islam@gmail.com>
2024-12-17 23:57:30 +0600

    I have created a simple to do list

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("input");
+const ul = document.querySelector("ul");
+const li = document.getElementsByClassName("li");
+const button = document.getElementsByTagName("button")[0];
+let data;
+input.addEventListener("input", (e) => {
+  data = e.target.value;
+});
+button.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.classList.add("li");
+  li.innerText = data;
+  ul.appendChild(li);
+  input.value = "";
+});
+ul.addEventListener("click", (e) => {
+  e.target.style.textDecoration = "line-through";
+  e.target.innerText = data + "  (Done)";
+});
+ul.addEventListener("dblclick", (e) => {
+  ul.removeChild(e.target);
+});
